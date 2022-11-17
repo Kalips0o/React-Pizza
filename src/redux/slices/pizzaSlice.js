99fb050ac79b8a224bc18b5ebfbc1a26a6bbf543
Kalips0o <Kalipsoo92@gmail.com>
2022-11-17 20:48:05 +0300

    thunkAPI

--- src/redux/slices/pizzaSlice.js
+++ src/redux/slices/pizzaSlice.js
@@ -2,15 +2,19 @@ import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import axios from "axios";
 
 export const fetchPizzas = createAsyncThunk(
-    'pizza/fetchPizzasStatus',
-    async (params) => {
-        const {sortBy, order, category, search, currentPage} = params;
-        const {data} = await axios.get(
-            `https://6356aacb2712d01e14f9c705.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
-        )
-        return data
+  "pizza/fetchPizzasStatus",
+  async (params, thunkAPI) => {
+    const { sortBy, order, category, search, currentPage } = params;
+    const { data } = await axios.get(
+      `https://6356aacb2712d01e14f9c705.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+    );
+    if (data.length === 0) {
+      return thunkAPI.rejectWithValue("Пиццы пустые");
     }
-)
+
+    return thunkAPI.fulfillWithValue(data);
+  }
+);
 
 
 const initialState = {
